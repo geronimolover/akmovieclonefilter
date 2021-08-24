01a048cb47fb27c4296226c4d93bf8a37ea4271d
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-08-24 20:04:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,7 +47,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("Here is your movie")
         
         if file_type == "document":
         
